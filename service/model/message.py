b34a965e42977a073eecfe068552299a1c11c1ab
daniel107x <DMejiaV310796@outlook.com>
2022-10-29 14:46:16 -0500

    Added db connection and message model

--- service/model/message.py
+++ service/model/message.py
@@ -1,3 +1,13 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 
-class Message:
-    pass
+Base = declarative_base()
+
+
+class Message(Base):
+    __tablename__ = "messages"
+    id = Column(Integer, primary_key=True)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    sensor_name = Column(String(40))
+    value = Column(Integer)
--- service/repository/repository.py
+++ service/repository/repository.py
@@ -1,9 +1,15 @@
+from service.database_service import DatabaseService
 from service.model.message import Message
 
 
 class Repository:
-    def __init__(self):
-        pass
+    def __init__(self, db: DatabaseService) -> None:
+        self._db = db
 
-    def save(self, message: Message):
-        pass
+    def create_tables(self):
+        Message.metadata.create_all(self._db.db_engine, checkfirst=True)
+
+    def save(self, message: Message) -> None:
+        self._db.save(message)
+
+
