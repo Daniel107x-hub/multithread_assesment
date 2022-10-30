b57d1d87b95ce661845682202911444a2947326d
daniel107x <DMejiaV310796@outlook.com>
2022-10-30 13:48:47 -0600

    Implemente consumer(Logger) logic

--- logger/logger.py
+++ logger/logger.py
@@ -6,13 +6,21 @@ from utils.network import Network
 
 class Logger(threading.Thread):
     def __init__(self, repository: Repository, network: Network, id: int):
+        super().__init__()
         self.repository: Repository = repository
         self.network: Network = network
         self.id = id
 
     def run(self) -> None:
         print(f"Starting consumer: {self.id}")
-        while True:
-            message = self.network.consume()
-            print(f"Consumer {self.id} got message: {message}")
-            # self.repository.save()
+        try:
+            while True:
+                message = self.network.consume()
+                if message is None:
+                    self.network.publish(None)
+                    break
+                print(f"Consumer {self.id} got message: {message}")
+                self.repository.save(message)
+            print(f"Stopping consumer {self.id}")
+        finally:
+            print(f"Stopped consumer {self.id}")
