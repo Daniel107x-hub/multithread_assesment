ed96fe8e26ff87e64ceb643df7fadaa1fecf2aa7
daniel107x <DMejiaV310796@outlook.com>
2022-10-30 10:12:34 -0600

    Updated network class

--- utils/network.py
+++ utils/network.py
@@ -1,3 +1,12 @@
+from service.model.message import Message
+
 
 class Network:
-    pass
+    def __init__(self):
+        pass
+
+    def publish(self, message: Message) -> None:
+        pass
+
+    def read(self):
+        pass
