b104b9b265f4a3b7eca2f16c8c4be0438132b166
daniel107x <DMejiaV310796@outlook.com>
2022-10-30 13:48:06 -0600

    Added queue to network

--- utils/network.py
+++ utils/network.py
@@ -1,12 +1,13 @@
+import queue
 from service.model.message import Message
 
 
 class Network:
-    def __init__(self):
-        pass
+    def __init__(self, buffer: queue.Queue):
+        self._buffer = buffer
 
     def publish(self, message: Message) -> None:
-        pass
+        self._buffer.put(message)
 
-    def read(self):
-        pass
+    def consume(self):
+        return self._buffer.get()
