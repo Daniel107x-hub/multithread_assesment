719b5a9d42104da2f78136339619d10cb692986c
daniel107x <DMejiaV310796@outlook.com>
2022-10-30 13:49:29 -0600

    First working version

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+import queue
+import sys
 import threading
 
 from sensors.sensor_factory import BuildingSensorFactory, NatureSensorFactory
 from sensors.sensors import SensorType
+from utils.network import Network
+from service.database_service import DatabaseService
+from service.repository.repository import Repository
+from logger.logger import Logger
 
 
 if __name__ == '__main__':
+    # Set up repository
+    db = DatabaseService()
+    repository = Repository(db)
+    repository.create_tables()
+
+    # Setting up the network
+    buffer = queue.Queue()
+    network = Network(buffer)
+
+    # Creating sensors
     sensors = []
-    building_sensor_factory = BuildingSensorFactory()
-    nature_sensor_factory = NatureSensorFactory()
+    building_sensor_factory = BuildingSensorFactory(network)
+    nature_sensor_factory = NatureSensorFactory(network)
     sensors.append(building_sensor_factory.create("sensor_1", SensorType.SMOKE))
     sensors.append(building_sensor_factory.create("sensor_2", SensorType.HEAT))
     sensors.append(building_sensor_factory.create("sensor_3", SensorType.TEMPERATURE))
     sensors.append(nature_sensor_factory.create("sensor_4", SensorType.WATER))
     sensors.append(nature_sensor_factory.create("sensor_5", SensorType.HUMIDITY))
+
+    # Setting up consumers
+    consumers = [Logger(repository, network, 1), Logger(repository, network, 2), Logger(repository, network, 3), Logger(repository, network, 4), Logger(repository, network, 5)]
+    for consumer in consumers:
+        consumer.start()
+
+    # Running sensor tasks
     threads = []
     for sensor in sensors:
         thread = threading.Thread(target=sensor.run)
@@ -24,6 +47,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt as e:
         for sensor in sensors:
             sensor.is_running = False
+
         while threads:
             for thread in threads:
                 thread.join(0.2)
@@ -32,4 +56,16 @@ if __name__ == '__main__':
                 else:
                     print("Thread successfully joined")
                     threads.remove(thread)
+        # Stop consumers
+        network.publish(None)
+        while consumers:
+            for consumer in consumers:
+                consumer.join(0.2)
+                if consumer.is_alive():
+                    print("Consumer not ready to join...")
+                else:
+                    print("Consumer successfully joined")
+                    consumers.remove(consumer)
+
         print("Program stopped")
+        sys.exit(e)
